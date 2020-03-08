357d3365c8c5c506cffb4ed3561f5fe0b362ade8
Jakilutra <jakilutra@gmail.com>
2020-03-08 12:57:06 +0000

    Update pairings_constructor.js

--- pairings_constructor.js
+++ pairings_constructor.js
@@ -1,7 +1,7 @@
-document.documentElement.className = 'no-fouc';
+document.documentElement.className = "no-fouc";
 $(document).ready(function() {
 	startUp();
-	$('.no-fouc').removeClass('no-fouc');
+	$(".no-fouc").removeClass("no-fouc");
 });
 function startUp () {
 	// Declaring variables.
