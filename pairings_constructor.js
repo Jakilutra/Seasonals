5072bdbb351e8e7aaebf9a0fa3b3f65243ef4ac5
Jakilutra <jakilutra@gmail.com>
2020-03-10 12:43:17 +0000

    Update pairings_constructor.js

--- pairings_constructor.js
+++ pairings_constructor.js
@@ -182,8 +182,10 @@ function scale (multiplier) {
 	document.getElementById("512").innerHTML = "512";
 }
 function modifySize (size, atStartUp) {
-	var round = 0, currentRoundIndex = 0, maxRoundIndex = 0, parityIndex = 0;
-	currentRoundIndex = atStartUp ? Math.Floor(parseInt(localStorage.getitem("round"))/2)-1 : document.getElementById("round").options.selectedIndex;
+	var roundString = "", round = 0, currentRoundIndex = 0, maxRoundIndex = 0, parityIndex = 0;
+	roundString = localStorage.getItem("round");
+	roundString = roundString === null ? "2" : roundString;
+	currentRoundIndex = atStartUp ? Math.floor(parseInt(round)/2)-1 : document.getElementById("round").options.selectedIndex;
 	maxRoundIndex = (Math.log2(parseInt(size)) - 3);
 	parityIndex = document.getElementById("parity").options.selectedIndex;
 	currentRoundIndex = (currentRoundIndex > maxRoundIndex) ? maxRoundIndex: currentRoundIndex;
