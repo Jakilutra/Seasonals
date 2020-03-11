b843581c1353d297d7b3280ffc232c5907e23a32
Jakilutra <jakilutra@gmail.com>
2020-03-11 21:20:18 +0000

    Update pairings_constructor.js

--- pairings_constructor.js
+++ pairings_constructor.js
@@ -430,8 +430,8 @@ function tidy (name, text) {
 		h6a = text.match(/Right\s\/\sBottom\sLoser\sBracket\n/);
 		linecount -= (h5a !== null) ? h5a.length/2 : 0;
 		linecount -= (h5a !== null) ? h6a.length/2 : 0;
-		linecount -= text.test(/One or more text areas do not contain the specified number of pairings\./)/2;
-		linecount -= text.test(/One or more pairings have names not prefixed with @\./)/2;
+		linecount -= + text.test(/One or more text areas do not contain the specified number of pairings\./)/2;
+		linecount -= + text.test(/One or more pairings have names not prefixed with @\./)/2;
 	}
 	document.getElementById(quad[name]).innerHTML = document.getElementById(quad[name]).innerHTML.replace(/\[[.\d]+\//,"["+linecount+"/");
 	if (text === "") {
