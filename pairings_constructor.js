4d1cfb098de60e46aa6a6a9d6ec9e5168125e7fc
Jakilutra <jakilutra@gmail.com>
2020-03-14 11:43:07 +0000

    Update pairings_constructor.js

--- pairings_constructor.js
+++ pairings_constructor.js
@@ -652,47 +652,21 @@ function output () {
 		return;
 	}
 	if (w1a.length === l1a.length && l1a.length > 1) {
-		i = 0;
-		while (m1a.length !== w1a.length) {
-			m1a.push(w1a[i]);
-			i++;
-			m1a.push(l1a[i]);
-			i++;
+		if (w1a.length === l1a.length && l1a.length > 1) {
+			for (i = 0; i < w1a.length; i += 2) {
+				m1a.push(w1a[i] + " ‹vs› " + l1a[i+1]);
+			}
+			for (i = 0; i < w1a.length; i += 2) {
+				m1a.push(l1a[i] + " ‹vs› " + w1a[i+1]);
+			}
 		}
-		i = 0;
-		while (m1a.length !== (w1a.length+l1a.length)) {
-			m1a.push(l1a[i]);
-			i++;
-			m1a.push(w1a[i]);
-			i++;
-		}
-		i = 0;
-		while (m1a.length !== w1a.length) {
-			m1a[i] = m1a[i] + " ‹vs› " + m1a[i+1];
-			i++;
-			m1a.splice(i, 1);
-		}
-	}
-	if (w2a.length === l2a.length && l2a.length > 1) {
-		i = 0;
-		while (m2a.length !== w2a.length) {
-			m2a.push(w2a[i]);
-			i++;
-			m2a.push(l2a[i]);
-			i++;
-		}
-		i = 0;
-		while (m2a.length !== (w2a.length+l2a.length)) {
-			m2a.push(l2a[i]);
-			i++;
-			m2a.push(w2a[i]);
-			i++;
-		}
-		i = 0;
-		while (m2a.length !== w2a.length) {
-			m2a[i] = m2a[i] + " ‹vs› " + m2a[i+1];
-			i++;
-			m2a.splice(i, 1);
+		if (w2a.length === l2a.length && l2a.length > 1) {
+			for (i = 0; i < w2a.length; i += 2) {
+				m2a.push(w2a[i] + " ‹vs› " + l2a[i+1]);
+			}
+			for (i = 0; i < w2a.length; i += 2) {
+				m2a.push(l2a[i] + " ‹vs› " + w2a[i+1]);
+			}
 		}
 	}
 	outputText = "▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂\n";
